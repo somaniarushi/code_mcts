2a0611c983a671856aea5114e8155ee5dbcc4164
Arushi Somani <arushi@adept.ai>
2024-07-27 08:50:04 +0000

    check in state

--- run_mcts.py
+++ run_mcts.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 import math
+import re
 from typing import Any
 from typing import Dict
 from typing import List
@@ -37,6 +38,25 @@ def early_exit_reward_or_none(curr_text: str, solution_to_reward: dict[str, int]
     return None
 
 
+def get_reward_from_llm_value_network(state: str, model: LlamaSampler) -> float:
+    """
+    Asks the model to return a value between 0 and 100 for a given state. Then extracts that value from the response and
+    returns it as the reward. If no value is extracted, returns 0.
+    """
+    SCORE_MATCH_REGEX = r'Score:\s*(\d+)'
+    prompt = state + '\nGiven this is how far a student has gotten in their solution to the problem, score them on a scale of 0 to 100. Explain your reasoning then leave your score as Score: <score>. Be concise and clear.'
+    response = model(prompt)
+    completion = response['choices'][0]['message']['content']
+    score_match = re.search(SCORE_MATCH_REGEX, completion)
+    if score_match:
+        integer_score = int(score_match.group(1))
+        score = integer_score / 100
+    else:
+        score = 0.0
+    print(f'Score: {score}')
+    return score
+
+
 def append_to_jsonl(output_jsonl_path: str, task_id: str, completion: str, reward: int, rollout_counts_for_solution: int) -> None:
     with open(output_jsonl_path, 'a') as f:
         f.write(
@@ -61,11 +81,15 @@ def main(output_jsonl_path: str, max_rollouts: int) -> None:
     rollout_generator = LlamaSampler(max_tokens=200)
     problems = read_problems()
 
+    # Get the last 30 tasks
+    problems = dict(list(problems.items())[-20:])  # To test fast. Revert.
+
     for task_id, problem in tqdm(problems.items()):
 
         solution_to_reward = {}
 
-        prompt = problem['prompt']
+        prompt = 'Solve the problem below by completing the given function. Once completed, end your generation with ```. Do not generate more problems or tests.\n```python\n\n' + \
+            problem['prompt']
         root = Node('', math.log(1), prompt, None)  # <PD> is the root node
         rollout_counts_for_solution = -1
         for i in range(max_rollouts):
@@ -74,7 +98,6 @@ def main(output_jsonl_path: str, max_rollouts: int) -> None:
 
             # Add a visit to the current node
             curr.visits += 1
-
             # Part 1: Selection
             while len(curr.children) > 0:
                 # Select the best child node
@@ -83,19 +106,32 @@ def main(output_jsonl_path: str, max_rollouts: int) -> None:
                 curr.visits += 1
 
             # Part 2: Expansion
-            next_tokens_and_logprobs: list[tuple[list[str], list[float]]] = (
-                next_token_generator.get_next_tokens(curr.state)
-            )
-            next_sentences = [
-                ''.join(tokens)
-                for tokens, _ in next_tokens_and_logprobs
-            ]
+            next_tokens_and_logprobs: list[
+                tuple[list[str], list[float]]
+            ] = next_token_generator.get_next_tokens(curr.state)
+            # If any are None, skip this rollout
+            if any(x is None or y is None for x, y in next_tokens_and_logprobs):
+                print(f'Skipping rollout {i + 1} for task {task_id}')
+                continue
+
+            next_sentences = []
             summed_logprobs = []
-            for _, logprob in next_tokens_and_logprobs:
-                summed_logprobs.append(sum(logprob))
-            next_tokens_and_logprobs = list(
-                zip(next_sentences, summed_logprobs),
-            )
+            try:
+                next_sentences = [
+                    ''.join(tokens)
+                    for tokens, _ in next_tokens_and_logprobs
+                ]
+                summed_logprobs = [
+                    sum(logprob)
+                    for _, logprob in next_tokens_and_logprobs
+                ]
+                next_tokens_and_logprobs = list(
+                    zip(next_sentences, summed_logprobs),
+                )
+            except Exception as e:
+                print(f'Error in getting next tokens: {e}')
+                continue
+
             # Optimization, dedup on the next tokens
             deduped_next_tokens_and_logprobs = []
             seen = set()
@@ -105,11 +141,9 @@ def main(output_jsonl_path: str, max_rollouts: int) -> None:
                     seen.add(token)
             next_tokens_and_logprobs = deduped_next_tokens_and_logprobs
 
-            # print(f"Next tokens: {next_tokens} with curr_state: \n```\n{curr.state}\n```\n")
             # Create child nodes
             child_nodes = [
-                Node(token, logprob, curr.state + token, curr)
-                for token, logprob in next_tokens_and_logprobs
+                Node(token, logprob, curr.state + token, curr) for token, logprob in next_tokens_and_logprobs
             ]
             curr.children = child_nodes
 
@@ -117,23 +151,27 @@ def main(output_jsonl_path: str, max_rollouts: int) -> None:
             # Early exit if a solution is found
             reward = early_exit_reward_or_none(curr.state, solution_to_reward)
             if reward is None:
-                rollout = curr.state + rollout_generator.generate(curr.state)
-                # Remove the prompt for evaluation
-                completion = rollout.replace(prompt, '')
-                completion = clean_completion(completion)
-                # 1 if correct, 0 otherwise
-                reward = evaluate_completion(completion, problem)
-                if reward == 1:
+                direct_reward = evaluate_completion(
+                    clean_completion(curr.state.replace(prompt, '')), problem,
+                )
+                if direct_reward == 1:
                     print(f'Found solution in {i + 1} steps!')
-                    solution_to_reward[rollout] = reward
+                    solution_to_reward[curr.state] = direct_reward
+                    reward = direct_reward
                 else:
-                    pass
-                    # print(f"===============================")
-                    # print(f"Prefix for task {task_id}:\n```\n{curr.state}\n```\n")
-                    # print(f"----")
-                    # print(f"Completion for task {task_id}:\n```\n{completion}\n```\n UNSUCCESSFUL with reward {reward}")
-                    # print(f"===============================")
-                # TODO: Can be more fine-grained?
+                    # Predict the reward using LLM as a value network
+                    reward = get_reward_from_llm_value_network(
+                        curr.state, rollout_generator,
+                    )
+                    print(f'===============================')
+                    print(
+                        f'Prefix for task {task_id}:\n```\n{curr.state}\n```\n',
+                    )
+                    print(f'----')
+                    print(
+                        f'State for task {task_id}:\n```\n{curr.state}\n```\n UNSUCCESSFUL with model reward {reward} | direct reward {direct_reward}',
+                    )
+                    print(f'===============================')
 
             # Part 4: Backpropagation
             curr.backprop(reward)
@@ -157,7 +195,6 @@ def main(output_jsonl_path: str, max_rollouts: int) -> None:
             output_jsonl_path, task_id, best_completion,
             best_reward, rollout_counts_for_solution,
         )
-        print_tree(root, problem['prompt'])
 
     print(f'Output written to {output_jsonl_path}')
     print(f'Done!')
--- scripts/run_run_mcts.py
+++ scripts/run_run_mcts.py
@@ -2,4 +2,4 @@ from __future__ import annotations
 
 from run_mcts import main
 
-main('outputs/mcts_rewards_beta_with_intermediate_rewards_8rollout_sentence.jsonl', 8)
+main('outputs/mcts_rewards_beta_with_intermediate_rewards_64rollout_sentence.jsonl', 64)
